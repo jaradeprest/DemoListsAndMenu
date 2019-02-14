8a95be2e7cf650b1f30c0a88cd35ac019b7f37dd
jaradeprest <jaradeprest@gmail.com>
2019-02-14 10:57:16 +0100

    mopDAO: added mopkes to mopList.

---
+++ model/Mop.java
@@ -0,0 +1,31 @@
+package iam.deprest.demolistsandmenu.model;
+
+public class Mop {
+
+    private String mop;
+    private String clou;
+
+    public Mop() {
+    }
+
+    public Mop(String mop, String clou) {
+        this.mop = mop;
+        this.clou = clou;
+    }
+
+    public String getMop() {
+        return mop;
+    }
+
+    public void setMop(String mop) {
+        this.mop = mop;
+    }
+
+    public String getClou() {
+        return clou;
+    }
+
+    public void setClou(String clou) {
+        this.clou = clou;
+    }
+}
---
+++ model/MopDAO.java
@@ -0,0 +1,31 @@
+package iam.deprest.demolistsandmenu.model;
+
+import java.util.ArrayList;
+
+public class MopDAO {
+    private static final MopDAO ourInstance = new MopDAO();
+    //lijst met moppen, die dynamisch is (kunnen moppen toevoegen of verwijderen) => ArrayList!!! (array is exact aantal elementen, staat vast)
+    private ArrayList<Mop> mopList = new ArrayList<>();
+
+    public static MopDAO getInstance() {
+        return ourInstance;
+    }
+
+    private MopDAO() {
+        mopList.add(new Mop("Het is oud en het vliegt...", "Een Bejaardelaar"));
+        mopList.add(new Mop("Het is rood en vliegt door de klas...", "Een ongestelde vraag"));
+        mopList.add(new Mop("Wat is de gelijkens tussen Heineken en seks in een roeibootje...", "It's fucking close to water"));
+        mopList.add(new Mop("Het is oranje en als het regent is het weg...", "De mannen van de gemeente"));
+        mopList.add(new Mop("Het is geel en weegt niet veel...", "Lichtgeel"));
+        mopList.add(new Mop("Waarom betaalt een lul geen belastingen?", "Hij is aftrekbaar"));
+        mopList.add(new Mop("Het is groen en heeft een gewei...", "Een dophertje"));
+        mopList.add(new Mop("Wat is een heks in de woestijn?", "Een sandwich"));
+        mopList.add(new Mop("Wat is een jood met een gasfles?", "Een doe-het-zelfer"));
+        mopList.add(new Mop("Wat is een jood met twee gasflessen?", "Een die-hard"));
+        mopList.add(new Mop("Hoe heet de bovenste steen van een huis?", "De schoorsteen"));
+    }
+
+    public ArrayList<Mop> getMopList() {
+        return mopList;
+    }
+}
